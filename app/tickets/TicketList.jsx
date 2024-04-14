be49a7100c7396292d4fa2efd632140918c02361
Vaibhav Srivastava <vabnix@hotmail.com>
2024-04-13 23:42:31 -0500

    Adding Data fetch

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,23 @@
+async function getTickets(){
+    const res = await fetch('http://localhost:4000/tickets')
+    return res.json();
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets();
+
+  return (
+    <>
+        {tickets.map(ticket => (
+            <div className="card my-5" key={ticket.id}>
+                <h3>{ticket.title}</h3>
+                <p>{ticket.body.slice(0,100)}...</p>
+                <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+            </div>
+        ))}
+        {tickets.length == 0 && (
+            <p className="text-center">No Tickets to show</p>
+        )}
+    </>
+  )
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,8 +1,10 @@
+import TicketList from "./TicketList";
 
 export default function Tickets() {
   return (
     <div>
       <h2>Tickets</h2>
+      <TicketList/>
     </div>
   )
 }
